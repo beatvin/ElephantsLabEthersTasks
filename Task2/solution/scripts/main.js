f40c8e943b97848173be361ab08a393f7aee60a2
beatvin <beatvin128@gmail.com>
2022-09-20 14:04:46 +0300

    task2

---
+++ Task2/solution/scripts/main.js
@@ -0,0 +1,29 @@
+import {ethers} from "./ethers-5.6.esm.min.js";
+
+let INFURA_API_KEY="XXXXXXXXXXX"; // paste your infura api key here
+
+document.getElementById("sendEtheButton").addEventListener("click", sendEth);
+
+async function sendEth() {
+
+    let privateKey = document.getElementById("privateKey").value;
+
+    let transferToAddress = document.getElementById("transferToAddress").value;
+
+    let amountInEth = document.getElementById("amountInEth").value;    
+
+    let provider = new  ethers.providers.InfuraProvider("goerli",INFURA_API_KEY); 
+
+    let wallet =  new ethers.Wallet(privateKey,provider);
+
+    let transaction = {
+        to: transferToAddress,
+        value: ethers.utils.parseEther(amountInEth)
+    }
+
+    let transactionResult = await wallet.sendTransaction(transaction);
+
+    alert(transactionResult.hash)
+}
+
+
